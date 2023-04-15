7d1d1daf2c964bdaf1104946a1dceb7c40212951
jenya-b <evgeny_rab@inbox.ru>
2023-04-15 12:08:16 +0300

    feat: add apollo-server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const expressPlayground =
+  require('graphql-playground-middleware-express').default;
+const { readFileSync } = require('fs');
+
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
+const resolvers = require('./resolvers');
+
+async function start() {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    )
+  );
+}
+
+start();
---
+++ resolvers/Query.js
@@ -0,0 +1,5 @@
+const Query = {
+  totalUsers: () => 42,
+};
+
+module.exports = Query;
