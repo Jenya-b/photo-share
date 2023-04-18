537f8db87e54884747ea94612d09b659d862f827
jenya-b <evgeny_rab@inbox.ru>
2023-04-18 23:25:43 +0300

    feat: send token with queries

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_API,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_API,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
